b0dbe01174f08f4ef1f03e89daccc7433ae368f7
Midyafn <80409196+Midyafn@users.noreply.github.com>
2021-04-13 09:03:38 +0800

    Update home.blade.php

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,23 +19,5 @@
     </div>
 </div>
 
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12 mb-2">
-        @foreach ($products as $product)
-        <div class="col-md-2">
-        <div class="card" style="width: 20rem;">
-            <img src="{{ url('uploads')}}/ {{ $product->image}}" class="card-img-top" alt="...">
-                <div class="card-body">
-                <h5 class="card-title">{{ $product->product_name}}</h5>
-                <p class="card-text">Rp. {{ number_format($product->price)}}</p>
-                <a href="#" class="btn btn-primary">More Info</a>
-                </div>
-            </div>
-            </div> 
-        </div>
-        @endforeach
-        </div>
-    </div>
-</div>
+
 @endsection
