728b22dcc4aca534bf26b3277b05c80d6b73c425
Midyafn <80409196+Midyafn@users.noreply.github.com>
2021-04-20 16:15:18 +0800

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -2,24 +2,51 @@
 
 @section('content')
 
-<div class="container">
+
+    <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12 mb-2">
         @foreach ($products as $product)
-        <div class="col-md-2">
-        <div class="card" style="width: 20rem;">
-            <img src="{{ url('uploads') }}/ {{ $products->images}}" class="card-img-top" alt="...">
-                <div class="card-body">
+        <div class="col-md-4">
+            <div class="card">
+              <img src="{{ url('uploads') }}/{{ $product->images }}" class="card-img-top" alt="...">
+              <hr>
+              <div class="card-body">
                 <h5 class="card-title">{{ $product->product_name}}</h5>
-                <p class="card-text">Rp. {{ number_format($product->price)}}</p>
-                <a href="#" class="btn btn-primary">ADD TO CART</a>
-                </div>
-            </div>
+                <strong>Price :</strong> Rp. {{ number_format($product->price)}} <br>
+                <hr>
+                    
+                    <!-- Button trigger modal -->
+                    <button type="button" class="btn btn-primary btn-lg active" data-toggle="modal" data-target="#exampleModal">
+                        Description
+                    </button>
+                    <a href="{{ url('cart') }}/{{ $product->id }}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Add to cart</a>
+
+                    <!-- Modal -->
+                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                        <div class="modal-dialog">
+                            <div class="modal-content">
+                            <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Description</h5>
+                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
+                            <div class="modal-body">
+                                {{ $product->description}}
+                            </div>
+                            <div class="modal-footer">
+                                <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                            </div>
+                            </div>
+                        </div>
+                    </div>
+
+              </div>
             </div> 
         </div>
         @endforeach
-        </div>
     </div>
 </div>
+
 
 @endsection
