0cd42a1cd59ec382074fb6fa7528e2d9af9a2f06
jayaantara10 <jayaantara10@gmail.com>
2021-04-26 10:07:09 +0800

    Perbaikan Admin dan view Admin
    Benerin Admiin
    -hapus bagian forget password
    
    Benerin view admin login
    -benerin indentasi
    -hapus bagian forget password
    -benerin bug tampilan

--- app/Admin.php
+++ app/Admin.php
@@ -38,8 +38,4 @@ class Admin extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    protected function sendPasswordResetNotification($token)
-    {
-        $this->notify(new AdminResetPasswordNotification($token) );
-    }
 }
--- resources/views/layouts/admin-app.blade.php
+++ resources/views/layouts/admin-app.blade.php
@@ -20,6 +20,92 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 </head>
 <body>
-@yield('content')
+    <div id="app">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">LookUp</a>
+        <i class="fa fa-level-up" aria-hidden="true"></i>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Wanita
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="/products">Pakaian</a>
+                            <a class="dropdown-item" href="#">Sepatu</a>
+                            <a class="dropdown-item" href="#">Aksesoris</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            Pria
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="#">Pakaian</a>
+                            <a class="dropdown-item" href="#">Sepatu</a>
+                            <a class="dropdown-item" href="#">Aksesoris</a>
+                            </div>
+                        </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+                        <button class="btn btn-outline-light my-2 my-sm-0" type="submit">Search</button>
+                        </form>
+                    </div>
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            </li>
+                            @if (Route::has('register'))
+                                <li class="nav-item">
+                                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                                </li>
+                            @endif
+                        @else
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
+                                </a>
+                                @if(\Illuminate\Support\Facades\Auth::guard('admin')->check())
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('admin.logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('admin-logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="admin-logout-form" action="{{ route('admin.logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                @else
+                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="{{ route('logout') }}"
+                                        onclick="event.preventDefault();
+                                                        document.getElementById('user-logout-form').submit();">
+                                            {{ __('Logout') }}
+                                        </a>
+
+                                        <form id="user-logout-form" action="{{ route('user.logout') }}" method="POST" class="d-none">
+                                            @csrf
+                                        </form>
+                                    </div>
+                                @endif
+                            </li>
+                        @endguest
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    </div>
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
 </body>
 </html>
